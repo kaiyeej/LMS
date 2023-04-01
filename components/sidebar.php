0f9e7c32619b304c94387ccc0b2fc5a5372aa1e1
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-04-01 16:09:05 +0800

    Sidebar

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,18 +1,18 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-        <a href="./">PMS</a>
+        <a href="./">LMS</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-        <a href="./">PMS</a>
+        <a href="./">LMS</a>
     </div>
     <ul class="sidebar-menu">
         <li><a class="nav-link" href="homepage"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
         <li class="menu-header">Master Data</li>
         <li><a class="nav-link" href="clients"><i class="fas fa-address-card"></i> <span>Clients</span></a></li>
-        <li><a class="nav-link" href="loan-types"><i class="fas fa-address-card"></i> <span>Loan Types</span></a></li>
+        <li><a class="nav-link" href="loan-types"><i class="fas fa-list"></i> <span>Loan Types</span></a></li>
 
         <li class="menu-header">Transactions</li>
-        <li><a class="nav-link" href="Loans"><i class="fas fa-book"></i> <span>Loans</span></a></li>
+        <li><a class="nav-link" href="Loans"><i class="fa fa-folder"></i> <span>Loans</span></a></li>
         <!-- <li class="dropdown">
             <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Menu 3</span></a>
             <ul class="dropdown-menu">
@@ -22,9 +22,9 @@
         </li> -->
 
         <li class="menu-header">Reports</li>
-        <li><a class="nav-link" href="aging-accounts"><i class="fas fa-address-card"></i> <span>Aging of Accounts</span></a></li>
-        <li><a class="nav-link" href="accounts-receivable"><i class="fas fa-address-card"></i> <span>Accounts Receivables</span></a></li>
-        <li><a class="nav-link" href="collections"><i class="fas fa-address-card"></i> <span>Collections</span></a></li>
+        <li><a class="nav-link" href="aging-accounts"><i class="fas fa-align-left"></i> <span>Aging of Accounts</span></a></li>
+        <li><a class="nav-link" href="accounts-receivable"><i class="fas fa-chart-bar"></i> <span>Accounts Receivables</span></a></li>
+        <li><a class="nav-link" href="collections"><i class="fas fa-clipboard-list"></i> <span>Collections</span></a></li>
         <!-- <li class="dropdown">
             <a href="#" class="nav-link has-dropdown"><i class="far fa-bar-graph"></i> <span>Aging of Accounts</span></a>
             <ul class="dropdown-menu">
--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if (!isset($_SESSION["pms_status"])) {
           </li>
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
               <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-              <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+              <div class="d-sm-none d-lg-inline-block">Hi, Juan Dela Cruz</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
               <a href="features-profile.html" class="dropdown-item has-icon">
