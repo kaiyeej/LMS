46c75d5b7aa5afbb69bc7e90f201ee5a3a0c13c5
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-04-01 15:51:59 +0800

    Update sidebar.php

--- components/sidebar.php
+++ components/sidebar.php
@@ -8,26 +8,30 @@
     <ul class="sidebar-menu">
         <li><a class="nav-link" href="homepage"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
         <li class="menu-header">Master Data</li>
-        <li><a class="nav-link" href="clients"><i class="fas fa-address-card"></i> <span>Menu 1</span></a></li>
+        <li><a class="nav-link" href="clients"><i class="fas fa-address-card"></i> <span>Clients</span></a></li>
+        <li><a class="nav-link" href="loan-types"><i class="fas fa-address-card"></i> <span>Loan Types</span></a></li>
 
         <li class="menu-header">Transactions</li>
-        <li><a class="nav-link" href="projects"><i class="fas fa-book"></i> <span>Menu 2</span></a></li>
-        <li class="dropdown">
+        <li><a class="nav-link" href="Loans"><i class="fas fa-book"></i> <span>Loans</span></a></li>
+        <!-- <li class="dropdown">
             <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Menu 3</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="submenu1">Submenu 1</a></li>
                 <li><a class="nav-link" href="submenu2">Submenu 2</a></li>
             </ul>
-        </li>
+        </li> -->
 
         <li class="menu-header">Reports</li>
-        <li class="dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="far fa-bar-graph"></i> <span>Report 1</span></a>
+        <li><a class="nav-link" href="aging-accounts"><i class="fas fa-address-card"></i> <span>Aging of Accounts</span></a></li>
+        <li><a class="nav-link" href="accounts-receivable"><i class="fas fa-address-card"></i> <span>Accounts Receivables</span></a></li>
+        <li><a class="nav-link" href="collections"><i class="fas fa-address-card"></i> <span>Collections</span></a></li>
+        <!-- <li class="dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="far fa-bar-graph"></i> <span>Aging of Accounts</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="submenu1">Submenu 1</a></li>
                 <li><a class="nav-link" href="submenu2">Submenu 2</a></li>
             </ul>
-        </li>
+        </li> -->
 
         <li class="menu-header">Security</li>
         <li><a class="nav-link" href="users"><i class="fas fa-user-cog"></i> <span>Users</span></a></li>
