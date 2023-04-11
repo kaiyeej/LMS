2384df5d017f6292932ca30ba69c006287169013
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-04-11 23:06:14 +0800

    Client
     - wizard in clients
    - added db

--- db/lms_db.sql
+++ db/lms_db.sql
@@ -1,22 +1,35 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2023 at 05:04 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table lms_db.tbl_clients
-CREATE TABLE IF NOT EXISTS `tbl_clients` (
-  `client_id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Database: `lms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_clients`
+--
+
+CREATE TABLE `tbl_clients` (
+  `client_id` int(11) NOT NULL,
   `client_fname` varchar(50) NOT NULL,
   `client_mname` varchar(50) NOT NULL,
   `client_lname` varchar(50) NOT NULL,
@@ -51,32 +64,41 @@ CREATE TABLE IF NOT EXISTS `tbl_clients` (
   `client_no_of_childred` int(3) NOT NULL DEFAULT 0,
   `client_no_of_child_dependent` int(3) NOT NULL DEFAULT 0,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_clients: ~1 rows (approximately)
+--
+-- Dumping data for table `tbl_clients`
+--
+
 INSERT INTO `tbl_clients` (`client_id`, `client_fname`, `client_mname`, `client_lname`, `client_name_extension`, `client_dob`, `client_contact_no`, `client_civil_status`, `client_address`, `client_address_status`, `client_res_cert_no`, `client_res_cert_issued_at`, `client_res_cert_date`, `client_employer`, `client_employer_address`, `client_employer_contact_no`, `client_emp_position`, `Column 18`, `client_emp_income`, `client_emp_status`, `client_emp_length`, `client_prev_emp`, `client_spouse`, `client_spouse_address`, `client_spouse_res_cert_no`, `client_spouse_res_cert_issued_at`, `client_no_of_child`, `client_no_of_child_elem`, `client_no_of_child_hs`, `client_no_of_child_college`, `client_spouse_res_cert_date`, `client_spouse_employer`, `client_no_of_childred`, `client_no_of_child_dependent`, `date_added`, `date_last_modified`) VALUES
-	(1, 'Sample', 'Mi', 'S', 'Jr.', '2008-12-29', '2313123', '', 'sdsa', '', '', '', '0000-00-00', '', '', '', '', '', 0.000, '', 0, '', '', '', '', '', 0, 0, 0, 0, '0000-00-00', '', 0, 0, '2023-04-09 21:09:35', '2023-04-09 21:31:43');
+(1, 'Sample', 'Mi', 'S', 'Jr.', '2008-12-29', '2313123', '', 'sdsa', '', '', '', '0000-00-00', '', '', '', '', '', '0.000', '', 0, '', '', '', '', '', 0, 0, 0, 0, '0000-00-00', '', 0, 0, '2023-04-09 21:09:35', '2023-04-09 21:31:43');
 
--- Dumping structure for table lms_db.tbl_collections
-CREATE TABLE IF NOT EXISTS `tbl_collections` (
-  `collection_id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_collections`
+--
+
+CREATE TABLE `tbl_collections` (
+  `collection_id` int(11) NOT NULL,
   `loan_id` int(11) DEFAULT NULL,
   `amount` decimal(12,3) DEFAULT NULL,
   `remarks` varchar(250) DEFAULT NULL,
   `status` varchar(1) NOT NULL COMMENT 'P - Pending ; F - Finished',
   `user_id` int(11) NOT NULL DEFAULT 0,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`collection_id`)
+  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_collections: ~0 rows (approximately)
+-- --------------------------------------------------------
 
--- Dumping structure for table lms_db.tbl_loans
-CREATE TABLE IF NOT EXISTS `tbl_loans` (
-  `loan_id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `tbl_loans`
+--
+
+CREATE TABLE `tbl_loans` (
+  `loan_id` int(11) NOT NULL,
   `reference_number` varchar(50) NOT NULL,
   `client_id` int(11) NOT NULL DEFAULT 0,
   `loan_type_id` int(11) NOT NULL DEFAULT 0,
@@ -87,32 +109,46 @@ CREATE TABLE IF NOT EXISTS `tbl_loans` (
   `status` varchar(1) NOT NULL DEFAULT 'P' COMMENT 'P - Pending; A - Approved; R - Released; D - Denied',
   `loan_date` date NOT NULL,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`loan_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_loans: ~1 rows (approximately)
+--
+-- Dumping data for table `tbl_loans`
+--
+
 INSERT INTO `tbl_loans` (`loan_id`, `reference_number`, `client_id`, `loan_type_id`, `loan_amount`, `loan_period`, `loan_interest`, `due_date`, `status`, `loan_date`, `date_added`, `date_last_modified`) VALUES
-	(1, 'LN-20230410152538', 1, 1, 1000.000, 4, 3.000, '0000-00-00', 'C', '2023-04-10', '2023-04-10 22:06:05', '2023-04-10 22:21:13');
+(1, 'LN-20230410152538', 1, 1, '1000.000', 4, '3.000', '0000-00-00', 'C', '2023-04-10', '2023-04-10 22:06:05', '2023-04-10 22:21:13');
 
--- Dumping structure for table lms_db.tbl_loan_types
-CREATE TABLE IF NOT EXISTS `tbl_loan_types` (
-  `loan_type_id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_loan_types`
+--
+
+CREATE TABLE `tbl_loan_types` (
+  `loan_type_id` int(11) NOT NULL,
   `loan_type` varchar(50) DEFAULT NULL,
   `loan_type_interest` decimal(5,2) NOT NULL,
   `remarks` varchar(250) DEFAULT NULL,
   `date_added` datetime DEFAULT current_timestamp(),
-  `date_last_modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`loan_type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `date_last_modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_loan_types: ~1 rows (approximately)
+--
+-- Dumping data for table `tbl_loan_types`
+--
+
 INSERT INTO `tbl_loan_types` (`loan_type_id`, `loan_type`, `loan_type_interest`, `remarks`, `date_added`, `date_last_modified`) VALUES
-	(1, 'short term loan', 12.00, '', '2023-04-09 22:12:49', '2023-04-11 10:22:37');
+(1, 'short term loan', '12.00', '', '2023-04-09 22:12:49', '2023-04-11 10:22:37');
 
--- Dumping structure for table lms_db.tbl_users
-CREATE TABLE IF NOT EXISTS `tbl_users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `user_id` int(11) NOT NULL,
   `user_fname` varchar(50) NOT NULL,
   `user_mname` varchar(50) NOT NULL,
   `user_lname` varchar(50) NOT NULL,
@@ -120,17 +156,86 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
   `username` varchar(20) NOT NULL,
   `password` text NOT NULL,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_users: ~2 rows (approximately)
+--
+-- Dumping data for table `tbl_users`
+--
+
 INSERT INTO `tbl_users` (`user_id`, `user_fname`, `user_mname`, `user_lname`, `user_category`, `username`, `password`, `date_added`, `date_last_modified`) VALUES
-	(1, 'Juan', '', 'Dela Cruz', 'A', 'admin', '0cc175b9c0f1b6a831c399e269772661', '2023-04-09 20:44:25', '2023-04-09 20:45:02'),
-	(2, 'd', 'k', 'k', 'A', 's', '03c7c0ace395d80182db07ae2c30f034', '2023-04-10 10:14:42', '2023-04-10 10:14:42');
+(1, 'Juan', '', 'Dela Cruz', 'A', 'admin', '0cc175b9c0f1b6a831c399e269772661', '2023-04-09 20:44:25', '2023-04-09 20:45:02'),
+(2, 'd', 'k', 'k', 'A', 's', '03c7c0ace395d80182db07ae2c30f034', '2023-04-10 10:14:42', '2023-04-10 10:14:42');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_clients`
+--
+ALTER TABLE `tbl_clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `tbl_collections`
+--
+ALTER TABLE `tbl_collections`
+  ADD PRIMARY KEY (`collection_id`);
+
+--
+-- Indexes for table `tbl_loans`
+--
+ALTER TABLE `tbl_loans`
+  ADD PRIMARY KEY (`loan_id`);
+
+--
+-- Indexes for table `tbl_loan_types`
+--
+ALTER TABLE `tbl_loan_types`
+  ADD PRIMARY KEY (`loan_type_id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_clients`
+--
+ALTER TABLE `tbl_clients`
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_collections`
+--
+ALTER TABLE `tbl_collections`
+  MODIFY `collection_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_loans`
+--
+ALTER TABLE `tbl_loans`
+  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_loan_types`
+--
+ALTER TABLE `tbl_loan_types`
+  MODIFY `loan_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- pages/clients/modal_clients.php
+++ pages/clients/modal_clients.php
@@ -1,6 +1,6 @@
 <form method='POST' id='frm_submit'>
     <div class="modal fade" id="modalEntry" role="dialog" aria-labelledby="myModalLabel" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalLabel"><span class='ion-compose'></span> Add Entry</h5>
@@ -10,7 +10,7 @@
                 </div>
                 <div class="modal-body">
                     <input type="hidden" id="hidden_id" name="input[client_id]">
-                    <div class="form-row">
+                    <!-- <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>First Name</label>
                             <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client first name" name="input[client_fname]" id="client_fname" required>
@@ -39,13 +39,153 @@
                             <label>Address</label>
                             <textarea class="form-control input-item" placeholder="Address" name="input[client_address]" id="client_address" required></textarea>
                         </div>
+                    </div> -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="wizard-steps">
+                                        <div id="page_1" class="wizard-step wizard-step-active">
+                                            <div class="wizard-step-icon">
+                                                <i class="far fa-user"></i>
+                                            </div>
+                                            <div class="wizard-step-label">
+                                                User Account
+                                            </div>
+                                        </div>
+                                        <div id="page_2" class="wizard-step">
+                                            <div class="wizard-step-icon">
+                                                <i class="fas fa-box-open"></i>
+                                            </div>
+                                            <div class="wizard-step-label">
+                                                Create an App
+                                            </div>
+                                        </div>
+                                        <div id="page_3" class="wizard-step">
+                                            <div class="wizard-step-icon">
+                                                <i class="fas fa-server"></i>
+                                            </div>
+                                            <div class="wizard-step-label">
+                                                Server Information
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <form class="wizard-content mt-2">
+                                <div id="page_content_1" class="wizard-pane">
+                                    <div class="form-group row">
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">First name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_fname]" id="client_fname" required>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <label class="text-md-right text-left">Middle name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_mname]" id="client_mname">
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <label class="text-md-right text-left">Last name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_lname]" id="client_lname" required>
+                                        </div>
+                                        <div class="col-lg-2">
+                                            <label class="text-md-right text-left">Suffix</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Name Extension" name="input[client_name_extension]" id="client_name_extension">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Civil Status</label>
+                                            <select class="form-control select2 input-item" id="client_civil_status" name="input[client_civil_status]" style="width:100%;" required>
+                                                <option value="">Please Select</option>
+                                                <option value="Single">Single</option>
+                                                <option value="Married">Married</option>
+                                                <option value="Widowed">Widowed</option>
+                                                <option value="Seperated">Seperated</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Birthday</label>
+                                            <input type="date" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_dob]" id="client_dob" required>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Contact #</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client contact number" name="input[client_contact_no]" id="client_contact_no" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-lg-8">
+                                            <label class="text-md-right text-left">Address</label>
+                                            <textarea class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_civil_status]" id="client_civil_status" required></textarea>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Status</label>
+                                            <select class="form-control select2 input-item" id="client_address_status" name="input[client_address_status]" style="width:100%;" required>
+                                                <option value="">Please Select</option>
+                                                <option value="Owned">Owned</option>
+                                                <option value="Rented">Rented</option>
+                                                <option value="Free Use">Free Use</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Resident Certificate #</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Certificate number" name="input[client_res_cert_no]" id="client_res_cert_no" required>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Issued At</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Certificate issued at" name="input[client_res_cert_issued_at]" id="client_res_cert_issued_at" required>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">Certificate Date</label>
+                                            <input type="date" class="form-control input-item" autocomplete="off" placeholder="Certificate issued date" name="input[client_res_cert_date]" id="client_res_cert_date" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <!-- <div class="col-md-6">
+                                            
+                                        </div>
+                                        <div class="col-md-6 text-right">
+                                            <a href="#" class="btn btn-icon icon-right btn-primary"><i class="fas fa-arrow-left"></i> Back</a>
+                                            <a href="#" class="btn btn-icon icon-right btn-primary">Next <i class="fas fa-arrow-right"></i></a>
+                                        </div> -->
+                                    </div>
+                                </div>
+
+
+                                <div id="page_content_2" class="wizard-pane" style="display:none;">
+                                    <div class="form-group row">
+                                        <div class="col-lg-4">
+                                            <label class="text-md-right text-left">First name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_fname]" id="client_fname" required>
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <label class="text-md-right text-left">Middle name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_mname]" id="client_mname">
+                                        </div>
+                                        <div class="col-lg-3">
+                                            <label class="text-md-right text-left">Last name</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Client middle name" name="input[client_lname]" id="client_lname" required>
+                                        </div>
+                                        <div class="col-lg-2">
+                                            <label class="text-md-right text-left">Suffix</label>
+                                            <input type="text" class="form-control input-item" autocomplete="off" placeholder="Name Extension" name="input[client_name_extension]" id="client_name_extension">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+
                 </div>
                 <div class="modal-footer bg-whitesmoke br">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" id="btn_submit" class="btn btn-primary">
                         Save
-                    </button>
+                    </button> -->
+                    <a href="#" class="btn btn-icon icon-right btn-secondary"><i class="fas fa-arrow-left"></i> Previous</a>
+                    <a href="#" class="btn btn-icon icon-right btn-primary" onclick="nextPAge">Save and continue <i class="fas fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
