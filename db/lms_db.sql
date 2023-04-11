04ad0fe0afcdd5b98da15846808c0f32c1bad9de
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-04-11 16:01:34 +0800

    Update lms_db.sql

--- db/lms_db.sql
+++ db/lms_db.sql
@@ -26,34 +26,85 @@ CREATE TABLE IF NOT EXISTS `tbl_clients` (
   `client_mname` varchar(50) NOT NULL,
   `client_lname` varchar(50) NOT NULL,
   `client_name_extension` varchar(5) NOT NULL,
-  `client_address` varchar(250) NOT NULL,
-  `client_dob` date NOT NULL,
+  `client_birthdate` date NOT NULL,
   `client_contact_no` varchar(30) NOT NULL,
+  `client_civil_status` varchar(10) NOT NULL COMMENT 'Single, Married, Widowed, Seperated',
+  `client_address` varchar(250) NOT NULL,
+  `client_address_status` varchar(10) NOT NULL COMMENT 'Owned, Rented, Free use',
+  `client_res_cert_no` varchar(30) NOT NULL,
+  `client_res_cert_issued_at` varchar(30) NOT NULL,
+  `client_res_cert_date` date NOT NULL,
+  `client_employer` varchar(50) NOT NULL,
+  `client_employer_address` varchar(250) NOT NULL,
+  `client_employer_contact_no` varchar(15) NOT NULL,
+  `client_emp_position` varchar(30) NOT NULL,
+  `Column 18` varchar(30) NOT NULL,
+  `client_emp_income` decimal(12,3) NOT NULL DEFAULT 0.000,
+  `client_emp_status` varchar(15) NOT NULL,
+  `client_emp_length` int(2) NOT NULL DEFAULT 0,
+  `client_prev_emp` varchar(50) NOT NULL,
+  `client_spouse` varchar(50) NOT NULL,
+  `client_spouse_address` varchar(250) NOT NULL,
+  `client_spouse_res_cert_no` varchar(50) NOT NULL,
+  `client_spouse_res_cert_issued_at` varchar(50) NOT NULL,
+  `client_no_of_child` int(3) NOT NULL DEFAULT 0,
+  `client_no_of_child_elem` int(3) NOT NULL DEFAULT 0,
+  `client_no_of_child_hs` int(3) NOT NULL DEFAULT 0,
+  `client_no_of_child_college` int(3) NOT NULL DEFAULT 0,
+  `client_spouse_res_cert_date` date NOT NULL,
+  `client_spouse_employer` varchar(50) NOT NULL DEFAULT '',
+  `client_no_of_childred` int(3) NOT NULL DEFAULT 0,
+  `client_no_of_child_dependent` int(3) NOT NULL DEFAULT 0,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`client_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table lms_db.tbl_clients: ~1 rows (approximately)
-INSERT INTO `tbl_clients` (`client_id`, `client_fname`, `client_mname`, `client_lname`, `client_name_extension`, `client_address`, `client_dob`, `client_contact_no`, `date_added`, `date_last_modified`) VALUES
-	(1, 'Sample', 'Mi', 'S', 'Jr.', 'sdsa', '2008-12-29', '2313123', '2023-04-09 21:09:35', '2023-04-09 21:31:43');
+INSERT INTO `tbl_clients` (`client_id`, `client_fname`, `client_mname`, `client_lname`, `client_name_extension`, `client_birthdate`, `client_contact_no`, `client_civil_status`, `client_address`, `client_address_status`, `client_res_cert_no`, `client_res_cert_issued_at`, `client_res_cert_date`, `client_employer`, `client_employer_address`, `client_employer_contact_no`, `client_emp_position`, `Column 18`, `client_emp_income`, `client_emp_status`, `client_emp_length`, `client_prev_emp`, `client_spouse`, `client_spouse_address`, `client_spouse_res_cert_no`, `client_spouse_res_cert_issued_at`, `client_no_of_child`, `client_no_of_child_elem`, `client_no_of_child_hs`, `client_no_of_child_college`, `client_spouse_res_cert_date`, `client_spouse_employer`, `client_no_of_childred`, `client_no_of_child_dependent`, `date_added`, `date_last_modified`) VALUES
+	(1, 'Sample', 'Mi', 'S', 'Jr.', '2008-12-29', '2313123', '', 'sdsa', '', '', '', '0000-00-00', '', '', '', '', '', 0.000, '', 0, '', '', '', '', '', 0, 0, 0, 0, '0000-00-00', '', 0, 0, '2023-04-09 21:09:35', '2023-04-09 21:31:43');
+
+-- Dumping structure for table lms_db.tbl_collections
+CREATE TABLE IF NOT EXISTS `tbl_collections` (
+  `collection_id` int(11) NOT NULL AUTO_INCREMENT,
+  `loan_id` int(11) DEFAULT NULL,
+  `amount` decimal(12,3) DEFAULT NULL,
+  `remarks` varchar(250) DEFAULT NULL,
+  `status` varchar(1) NOT NULL COMMENT 'P - Pending ; F - Finished',
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`collection_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table lms_db.tbl_collections: ~0 rows (approximately)
 
 -- Dumping structure for table lms_db.tbl_loans
 CREATE TABLE IF NOT EXISTS `tbl_loans` (
   `loan_id` int(11) NOT NULL AUTO_INCREMENT,
   `reference_number` varchar(50) NOT NULL,
   `client_id` int(11) NOT NULL DEFAULT 0,
+  `loan_type_id` int(11) NOT NULL DEFAULT 0,
+  `loan_amount` decimal(12,3) NOT NULL DEFAULT 0.000,
+  `loan_period` int(6) NOT NULL DEFAULT 0,
+  `loan_interest` decimal(12,3) NOT NULL DEFAULT 0.000,
+  `due_date` date NOT NULL,
+  `status` varchar(1) NOT NULL DEFAULT 'P' COMMENT 'P - Pending; A - Approved; R - Released; D - Denied',
+  `loan_date` date NOT NULL,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`loan_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_loans: ~0 rows (approximately)
+-- Dumping data for table lms_db.tbl_loans: ~1 rows (approximately)
+INSERT INTO `tbl_loans` (`loan_id`, `reference_number`, `client_id`, `loan_type_id`, `loan_amount`, `loan_period`, `loan_interest`, `due_date`, `status`, `loan_date`, `date_added`, `date_last_modified`) VALUES
+	(1, 'LN-20230410152538', 1, 1, 1000.000, 4, 3.000, '0000-00-00', 'C', '2023-04-10', '2023-04-10 22:06:05', '2023-04-10 22:21:13');
 
 -- Dumping structure for table lms_db.tbl_loan_types
 CREATE TABLE IF NOT EXISTS `tbl_loan_types` (
   `loan_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `loan_type` varchar(50) DEFAULT NULL,
+  `loan_type_interest` decimal(5,2) NOT NULL,
   `remarks` varchar(250) DEFAULT NULL,
   `date_added` datetime DEFAULT current_timestamp(),
   `date_last_modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -61,8 +112,8 @@ CREATE TABLE IF NOT EXISTS `tbl_loan_types` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table lms_db.tbl_loan_types: ~1 rows (approximately)
-INSERT INTO `tbl_loan_types` (`loan_type_id`, `loan_type`, `remarks`, `date_added`, `date_last_modified`) VALUES
-	(1, 'h', '', '2023-04-09 22:12:49', '2023-04-09 22:13:21');
+INSERT INTO `tbl_loan_types` (`loan_type_id`, `loan_type`, `loan_type_interest`, `remarks`, `date_added`, `date_last_modified`) VALUES
+	(1, 'short term loan', 12.00, '', '2023-04-09 22:12:49', '2023-04-11 10:22:37');
 
 -- Dumping structure for table lms_db.tbl_users
 CREATE TABLE IF NOT EXISTS `tbl_users` (
@@ -76,11 +127,12 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lms_db.tbl_users: ~1 rows (approximately)
+-- Dumping data for table lms_db.tbl_users: ~2 rows (approximately)
 INSERT INTO `tbl_users` (`user_id`, `user_fname`, `user_mname`, `user_lname`, `user_category`, `username`, `password`, `date_added`, `date_last_modified`) VALUES
-	(1, 'Juan', '', 'Dela Cruz', 'A', 'admin', '0cc175b9c0f1b6a831c399e269772661', '2023-04-09 20:44:25', '2023-04-09 20:45:02');
+	(1, 'Juan', '', 'Dela Cruz', 'A', 'admin', '0cc175b9c0f1b6a831c399e269772661', '2023-04-09 20:44:25', '2023-04-09 20:45:02'),
+	(2, 'd', 'k', 'k', 'A', 's', '03c7c0ace395d80182db07ae2c30f034', '2023-04-10 10:14:42', '2023-04-10 10:14:42');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
