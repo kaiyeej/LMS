f977d6c22a09cb190ef6ef1641738960bace2bac
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2023-06-09 15:42:47 +0800

    Update class.chartclassification.php

--- core/classes/class.chartclassification.php
+++ core/classes/class.chartclassification.php
@@ -71,4 +71,30 @@ class ChartClassification extends Connection
         $row = $result->fetch_assoc();
         return $row['chart_class_code'];
     }
+
+    public function schema()
+    {
+        if (DEVELOPMENT) {
+            $default['date_added'] = $this->metadata('date_added', 'datetime', '', 'NOT NULL', 'CURRENT_TIMESTAMP');
+            $default['date_last_modified'] = $this->metadata('date_last_modified', 'datetime', '', 'NOT NULL', '', 'ON UPDATE CURRENT_TIMESTAMP');
+            $default['user_id'] = $this->metadata('user_id', 'int', 11);
+
+
+            // TABLE HEADER
+            $tables[] = array(
+                'name'      => $this->table,
+                'primary'   => $this->pk,
+                'fields' => array(
+                    $this->metadata($this->pk, 'int', 11, 'NOT NULL', '', 'AUTO_INCREMENT'),
+                    $this->metadata($this->name, 'varchar', 50),
+                    $this->metadata('chart_class_code', 'int', 11),
+                    $default['user_id'],
+                    $default['date_added'],
+                    $default['date_last_modified']
+                )
+            );
+
+            return $this->schemaCreator($tables);
+        }
+    }
 }
