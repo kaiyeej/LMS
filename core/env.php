a1a2537146d9d72ec8eb3fd616e5115d86c67132
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2023-07-11 09:41:06 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -3,14 +3,14 @@
 define('BASE_URL', 'http://localhost/lms/');
 define("BASE_PATH", __DIR__ . "/../");
 
-// define("HOST", "localhost");
-// define("USER", "root");
-// define("PASSWORD", "");
-// define("DBNAME", "lms_db");
-// define("DEVELOPMENT", true);
-
 define("HOST", "localhost");
-define("USER", "u981310152_lms_root");
-define("PASSWORD", "Lms123!!");
-define("DBNAME", "u981310152_lms_db");
+define("USER", "root");
+define("PASSWORD", "");
+define("DBNAME", "lms_db");
 define("DEVELOPMENT", true);
+
+// define("HOST", "localhost");
+// define("USER", "u981310152_lms_root");
+// define("PASSWORD", "Lms123!!");
+// define("DBNAME", "u981310152_lms_db");
+// define("DEVELOPMENT", true);
